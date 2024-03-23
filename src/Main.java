95fb58514838ee69701ad7c333f37da9cec22b42
Kasey Craig <kaseycraig@Kaseys-MacBook-Pro.local>
2024-03-23 15:24:56 +0100

    push to github

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,16 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public Main() {
-    }
 
     public static void main(String[] args) {
+
         int a = 5;
         int b = 6;
         int c = 1;
-        int[] taskOne = new int[]{a, b, c};
+        int[] taskOne = {a, b, c};
 
-        int i;
-        int j;
-        for(i = 0; i < taskOne.length; ++i) {
-            for(j = i + 1; j < taskOne.length; ++j) {
+        for(int i = 0; i < taskOne.length; i++) {
+            for(int j = i + 1; j < taskOne.length; j++) {
                 if (taskOne[i] > taskOne[j]) {
                     int temp = taskOne[i];
                     taskOne[i] = taskOne[j];
@@ -22,20 +19,27 @@ public class Main {
             }
         }
 
-        i = taskOne[0];
-        j = taskOne[1];
+        int taskOneMin = taskOne[0];
+        int taskTwo = taskOne[0+1];
         int taskOneMax = taskOne[taskOne.length - 1];
-        int diff = taskOneMax - i;
-        System.out.println("Difference between min and max is " + diff);
-        System.out.println("the smallest number in the array is " + i);
-        System.out.println("the second smallest number in the array is " + j);
-        float x = 5.0F;
-        float y = 5.0F;
-        float inside = 4.0F * x / 5.0F;
-        float insideY = inside - y;
-        float outside = insideY * insideY;
-        float firstX = x * x;
-        float var10000 = firstX + outside;
-        System.out.println("the results of task 3 is " + firstX);
+        int diff = taskOneMax - taskOneMin;{
+
+            System.out.println("Difference between min and max is " + diff);
+            System.out.println("the smallest number in the array is " + taskOneMin);
+            System.out.println("the second smallest number in the array is " + taskTwo);
+        }
+
+
+        //task 3
+        float x = 5;
+        float y = 6;
+        float inside = ( 4*y/5);
+        float insideY = (inside - x);
+        float outside = (insideY * insideY);
+        float firstX = (x*x);
+        float result = (firstX + outside);
+        {
+            System.out.println("the results of task 3 is " + result);
+        }
     }
 }
